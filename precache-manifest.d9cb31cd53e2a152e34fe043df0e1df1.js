269ff368797e406bca962d22e6997667651d0f5a
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-19 13:23:35 +0300

    Updates

---
+++ precache-manifest.d9cb31cd53e2a152e34fe043df0e1df1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4341aff71602337a5eee63acb7d1189",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "b523f5beb320e4261157",
+    "url": "/project/static/css/main.c7a9a11e.chunk.css"
+  },
+  {
+    "revision": "d538b57727b92f35342a",
+    "url": "/project/static/js/2.3c4200a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project/static/js/2.3c4200a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b523f5beb320e4261157",
+    "url": "/project/static/js/main.ca612fec.chunk.js"
+  },
+  {
+    "revision": "5748b1b47e5eacd52783",
+    "url": "/project/static/js/runtime-main.c1e01555.js"
+  },
+  {
+    "revision": "8d94767ca00cf43f37759914c5352eb4",
+    "url": "/project/static/media/ar-icon.8d94767c.svg"
+  },
+  {
+    "revision": "4b9079077a6275837fe774ce6f1f50a8",
+    "url": "/project/static/media/clothes.4b907907.svg"
+  },
+  {
+    "revision": "5d14e7e5475ebf20459756d6f9b93912",
+    "url": "/project/static/media/dots.5d14e7e5.svg"
+  },
+  {
+    "revision": "8bab732c217955f87f5a9ae437ce9469",
+    "url": "/project/static/media/food.8bab732c.svg"
+  },
+  {
+    "revision": "e873ac6fa3d2bc6d339102968c167c58",
+    "url": "/project/static/media/health.e873ac6f.svg"
+  },
+  {
+    "revision": "c5ab171491e5e06d50d9f1e8cf0885b1",
+    "url": "/project/static/media/other.c5ab1714.svg"
+  },
+  {
+    "revision": "dce15f4fe1e693b407e3f901503e5a3e",
+    "url": "/project/static/media/pet.dce15f4f.svg"
+  },
+  {
+    "revision": "f3f17d8d2e8067d8454a3fba12646bfd",
+    "url": "/project/static/media/restor.f3f17d8d.svg"
+  },
+  {
+    "revision": "cbff422cac5875b64b8e3937e9192155",
+    "url": "/project/static/media/smile.cbff422c.svg"
+  },
+  {
+    "revision": "205816934e67b7749f4dac7dfadca90b",
+    "url": "/project/static/media/transport.20581693.svg"
+  },
+  {
+    "revision": "7318d4c7150fcd208a1ee3169de56b06",
+    "url": "/project/static/media/utility.7318d4c7.svg"
+  }
+]);
