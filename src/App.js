88c2f8b6655ec2121071811f8e8761cf25374b67
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-28 03:28:20 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homePage';
-import SideManu from './sideManu';
-
+import React from "react";
+import "./App.css";
+import HomePage from "./homePage";
+import SideManu from "./sideManu";
 
 const App = () => (
   <div className="wrapper">
     <SideManu />
     <HomePage />
-    <div>"Yebat ty loh"</div>
   </div>
 );
-
 
 export default App;
