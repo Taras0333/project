50e3f0c7b25acfd09b9aa05bad4c2c6dc6fc83a7
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-28 03:49:13 +0300

    fff

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./homePage";
+import SideManu from "./sideManu";
+
+const App = () => (
+  <div className="wrapper">
+    <SideManu />
+    <HomePage />
+  </div>
+);
+
+export default App;
